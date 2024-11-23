775d3a34ed972d7dc814a21bdfa9a346fa728e0d
Mimizaya <miapdrsn@gmail.com>
2024-11-23 17:43:03 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/genshin-code-checker/', // GitHub repo name
+  server: {
+    host: true, // or use '0.0.0.0'
+    port: 5173, // or your preferred port
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  //assetsInclude: ['**/*.csv'],  // CSV files as assets
+})
